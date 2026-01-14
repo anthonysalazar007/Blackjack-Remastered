ee7b56358184eeedc3ffe3df38f673e92c60d0e7
ASalazar <anthonythesalazar028@gmail.com>
2026-01-13 18:19:31 -0800

    Imported Pandas and Seaborn, Removed duplicate game function

--- BlackJack_Remaster.py
+++ BlackJack_Remaster.py
@@ -2,7 +2,8 @@
 #To do; Figure out duplicate blackjack funcs
 #========================================
 import random
-
+import pandas as pd
+import seaborne as sns
 cards = ['Ace of Hearts', '2 of Hearts', '3 of Hearts', '4 of Hearts', '5 of Hearts', '6 of Hearts', '7 of Hearts', '8 of Hearts', '9 of Hearts', '10 of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts',
     'Ace of Diamonds', '2 of Diamonds', '3 of Diamonds', '4 of Diamonds', '5 of Diamonds', '6 of Diamonds', '7 of Diamonds', '8 of Diamonds', '9 of Diamonds', '10 of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds',
     'Ace of Clubs', '2 of Clubs', '3 of Clubs', '4 of Clubs', '5 of Clubs', '6 of Clubs', '7 of Clubs', '8 of Clubs', '9 of Clubs', '10 of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs',
@@ -13,28 +14,6 @@ playerWins = []
 houseWins = []
 ties = []
 playerChips = [20]
-
-def blackjack(): #Plays one round of blackjack when called upon
-    for i in range(2):
-        random.shuffle(cards)
-        playerCards = [cards.pop()]
-        playerCards.append(cards.pop())
-        houseCards = [cards.pop()]
-        houseCards.append(cards.pop())
-    
-    bet = input("How many chips will you bet?\n>>")   
-    print("Your hand is", playerCards[0], "and", playerCards[1])
-    call = input("What would you like to do? \n>>1. Stay\n>>2. Hit\n>>").lower()
-        
-    if call == "1" or call == "stay":
-        print("The House had", houseCards)
-    elif call == "2" or call == "hit":    
-        playerCards = [cards.pop()]
-        playerCards.append(cards.pop())
-        print("Your hand is", playerCards[0], "and", playerCards[1], "and", playerCards[2])
-
-
-# BEGINNING OF THE ACTUAL PROGRAM
 
 play = input("Hello! Would you like to play Blackjack?\n>>1. Yes\n>>2. No\n>>").lower()
 
